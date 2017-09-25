c8146589dca3d82e048df349f183fdcf4da5a8bc
annacrespo12 <annacrespo12@gmail.com>
2017-09-24 21:10:36 -0700

    added html files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+let app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "app/public")));
+
+// app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
