087d97642a4815dae961b7983f4d2eb067ced68d
annacrespo12 <annacrespo12@gmail.com>
2017-09-25 22:31:11 -0700

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,64 +19,64 @@ var friendsData = [
     "name":"Cheshire Cat",
     "photo":"https://upload.wikimedia.org/wikipedia/en/8/8e/DisneyCheshireCat.jpg",
     "scores":[
-        5,
-        1,
         4,
-        4,
-        5,
-        1,
+        2,
+        3,
         2,
         5,
-        4,
-        1
+        3,
+        1,
+        1,
+        2,
+        4
       ]
   },
   {
     "name":"Queen of Hearts",
     "photo":"https://upload.wikimedia.org/wikipedia/en/1/1c/DisneyQueenHearts.jpg",
     "scores":[
-        5,
         1,
-        4,
-        4,
-        5,
         1,
-        2,
-        5,
-        4,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
         1
       ]
   },
   {
     "name":"Mad Hatter",
-    "photo":"http://www.animationsensations.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/mad-hatter2.jpg",
+    "photo":"https://i.pinimg.com/736x/64/aa/c1/64aac14f48483095337e28013680bcc7--mad-hatter-costumes-mad-hatter-hats.jpg",
     "scores":[
         5,
-        1,
-        4,
-        4,
         5,
-        1,
-        2,
         5,
-        4,
-        1
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
       ]
   },
   {
     "name":"White Rabbit",
     "photo":"https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=11017832",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
       ]
   }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,37 +1,25 @@
+var _ = require("lodash");
 var friendsData = require("../data/friends.js");
 console.log(friendsData);
-module.exports = function(app) {
-	app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         console.log('retrieving freinds!');
         res.json(friendsData);
-	});
-	app.post('/api/friends', function(req, res) {
-        console.log('saving a new friend!!!');
-        friendsData.push(req.body);
-        console.log(friendsData);
-        res.json(friendsData);
-        friendsData.push(req.body);
-        var friend = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: req.body.scores
-        };
-
-        console.log(friend);
     });
-
-    // app.post('/api/clear', function() {
-    //     friendsData = [];
-    //     console.log(friendsData);
-    // });
-}
-//A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-// You should save your application's data inside of app/data/friends.js as an array of objects.
-
-//Determine the user's most compatible friend using the following as a guide:
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-// The closest match will be the user with the least amount of difference.
-// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-// The modal should display both the name and picture of the closest match.
+    app.post('/api/friends', function (req, res) {
+        var scores = req.body.scores;
+        var compareScores = [];
+        friendsData.forEach(function (friend) {
+            var friendScore = 0;
+            friend.scores.forEach(function (score, num) {
+                friendScore = friendScore + Math.abs(score - parseInt(scores[num]));
+            });
+            
+            compareScores.push({
+                "friend": friend,
+                "score": friendScore
+            });
+        });
+        res.json(_.sortBy(compareScores, "score").shift());
+    });
+  };
